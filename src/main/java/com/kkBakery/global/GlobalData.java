4dc59194e63900074a802f1c0970eea99b2797a2
Keshav <kingerkeshav@gmail.com>
2023-11-09 16:43:39 +0530

    First Commit

---
+++ src/main/java/com/kkBakery/global/GlobalData.java
@@ -0,0 +1,13 @@
+package com.kkBakery.global;
+
+import com.kkBakery.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalData {
+    public static List<Product> cart;
+    static {
+        cart = new ArrayList<Product>();
+    }
+}
---
+++ src/main/java/com/kkBakery/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.kkBakery.repository;
+
+import com.kkBakery.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
